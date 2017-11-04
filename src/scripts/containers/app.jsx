f11a97fb623f47ef2ac18ddf84071a0fe03d52db
Lav39 <lavkushkumar7654@gmail.com>
2017-11-04 09:48:53 -0700

    Created react workspace and added index.html

---
+++ src/scripts/containers/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);   
+    }   
+
+
+    render() {
+        
+        return (
+            <div id = 'app-container'>
+              Hello World !!
+            </div>
+        )
+    }
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src/scripts");
+
+var config = {
+    entry: SRC_DIR + "/utilities/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        rules: [{        
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "env", "stage-2"]
+                },
+            }]
+    }
+};
+
+module.exports = config;
